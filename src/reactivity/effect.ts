a75618b58d916b6e1b1b3c063c7e4e32b6347418
fmn <cdnxyf@gmail.com>
2022-06-11 18:42:22 +0800

    增加stop  scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,36 @@
-
+import { extend} from '../shared';
 
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn){
+    public scheduler;
+    deps = [];
+    active = true;
+    onStop?: () => void;
+    constructor(fn, scheduler?){
         this._fn = fn;
+        this.scheduler = scheduler;
     }
 
     run(){
         activeEffect = this;
-        this._fn();
+        return this._fn();
     }
+
+    stop(){
+        if (this.active) {
+            cleanupEffect(this);
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep:any) => {
+        dep.delete(effect);
+    });
 }
 
 const targetsMap = new Map();
@@ -29,19 +50,41 @@ export function track(target, key){
         dep  = new Set();
         depsMap.set(key, dep);
     }
+
+    if(!activeEffect) return;
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 } 
 
 export function trigger(target, key){
     let depsMap = targetsMap.get(target);
     let dep = depsMap.get(key);
     for (const effect of dep) {
-        effect.run();
+        if (effect.scheduler) {
+            effect.scheduler();
+        }else {
+            effect.run();
+        }
     }
 }
 
 let activeEffect;
-export function effect(fn){
-    const _effect = new ReactiveEffect(fn);
+export function effect(fn, options:any = {}){
+
+    const scheduler = options.scheduler;
+    const _effect = new ReactiveEffect(fn, scheduler);
+ 
+    // options extend
+    // _effect.onStop = options.onStop;
+    extend(_effect, options)
+
     _effect.run();
+
+    const runner:any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop(runner){
+    runner.effect.stop();
 }
