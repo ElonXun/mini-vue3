be970d647c60f5ce779ed11dd76d67b65654aeca
fmn <cdnxyf@gmail.com>
2022-06-12 20:57:15 +0800

    实现isProxy功能

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { readonly, isReadonly } from '../reactive';
+import { readonly, isReadonly, isProxy } from '../reactive';
 
 describe('readonly', () => {
     it('readonly part one', () => {
@@ -12,7 +12,7 @@ describe('readonly', () => {
         expect(isReadonly(original)).toBe(false);
         expect(isReadonly(wrapped.bar)).toBe(true);
         expect(isReadonly(original.bar)).toBe(false);
-
+        expect(isProxy(wrapped)).toBe(true);
     });
 
     it('readonly part two set warning ', () => {
