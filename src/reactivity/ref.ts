bdea50c1d2eaf1445a53497a335050e994b420c9
fmn <cdnxyf@gmail.com>
2022-06-13 21:18:32 +0800

    完善 ref 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,28 +1,48 @@
 import { trackEffects, triggerEffects, isTracking } from "./effect";
+import { hasChanged, isObject } from "../shared"; 
+import { reactive } from "./reactive";
 
-
+// ref 主要针对 单个值 string number boolean 监听 
+// reactive 主要是利用 object进行 proxy  不适用于单个string number boolean 的值监听
 class RefImpl {
     private _value: any;
     public dep;
+    private _rawValue: any;
     constructor(value){
-        this._value = value;
+        this._rawValue = value;
+        // 如果value是Object类型  先要把value值用reactive进行转化    
+        this._value = convert(value);
+
         this.dep = new Set();
     }
 
     get value(){
-        if (isTracking()) {
-            trackEffects(this.dep);
-        }
+        trackRefValue(this);
         return this._value;
     }
 
     set value(newValue){
-        // 先修改value值 再去触发依赖
-        this._value = newValue;
-        triggerEffects(this.dep);
+        // 如果newValue 和 oldValue 相同直接返回
+        // 注意 对比时 如果ref传入的是Object newValue(Object)和this._value(Proxy)为复杂对象 
+        if (hasChanged(newValue, this._rawValue)) {
+            // 先修改value值 再去触发依赖
+            this._rawValue = newValue;
+            this._value = convert(newValue);
+            triggerEffects(this.dep);
+        }
+
     }
 }
 
+function convert(value) {
+    return isObject(value) ? reactive(value) : value;
+}
+
+function trackRefValue(ref){
+    if (isTracking()) {
+        trackEffects(ref.dep);
+    }
+}
 
 export function ref (value) {
     return new RefImpl(value);
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -8,7 +8,7 @@ describe('ref', () => {
         expect(a.value).toBe(1);
     });
 
-    it('should be reactive', () => {
+    it('ref should be reactive', () => {
         const a = ref(1);
         let dummy;
         let calls = 0;
@@ -22,10 +22,22 @@ describe('ref', () => {
         expect(calls).toBe(2);
         expect(dummy).toBe(2);
         // same value should not trigger
-        // a.value = 2;
-        // expect(calls).toBe(2);
-        // expect(dummy).toBe(2);
+        a.value = 2;
+        expect(calls).toBe(2);
+        expect(dummy).toBe(2);
     });
 
+    it('ref should make nested properties reactive', () => {
+        const a = ref({
+            count: 1,
+        });
+        let dummy;
+        effect(() => {
+            dummy = a.value.count;
+        });
+        expect(dummy).toBe(1);
+        a.value.count = 2;
+        expect(dummy).toBe(2);
+    });
 
 });
